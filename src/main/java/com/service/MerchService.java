fbbeab9582714c51a145e6b3a97ec4c848f2babc
喬喬 <juliechu311@gmail.com>
2024-05-19 14:36:24 +0800

    調整名稱

--- src/main/java/com/service/MerchService.java
+++ src/main/java/com/service/MerchService.java
@@ -4,7 +4,6 @@ package com.service;
 import com.dao.MerchRepository;
 import com.entity.Merch;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
