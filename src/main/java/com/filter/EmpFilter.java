43e5bd092e83e85c13fe0d2e577948d931afab7d
DESKTOP-BSU46OA\a0939 <na0953866283@gmail.com>
2024-05-21 11:09:46 +0800

    排除阻擋所有css，js，圖片，與/front_end資料夾下所有的路徑

--- src/main/java/com/filter/EmpFilter.java
+++ src/main/java/com/filter/EmpFilter.java
@@ -1,6 +1,8 @@
 package com.filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -14,7 +16,9 @@ import java.util.Optional;
 
 @Component
 public class EmpFilter implements HandlerInterceptor {
-    String[] ignoreUrls = {"/emp/toLogin","/emp/doLogin","/emp/resetPasswordLink"};
+    String[] ignoreUrls = {"/emp/toLogin","/emp/doLogin","/emp/resetPasswordLink","/error","/front_end/**",
+            "/**/*.js", "/**/*.css", "/**/*.jpg", "/**/*.jpeg", "/**/*.png", "/**/*.gif", "/**/*.svg"};
+    private AntPathMatcher antPathMatcher = new AntPathMatcher();
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Cookie[] cookies = request.getCookies();
@@ -22,13 +26,16 @@ public class EmpFilter implements HandlerInterceptor {
         if (!ObjectUtils.isEmpty(cookies)) {
             loginAlready = Arrays.stream(cookies).filter(cookie -> "loginAlready".equals(cookie.getName())).findFirst();
         }
-        List<String> list = Arrays.stream(ignoreUrls).toList();
-        for (String url :ignoreUrls){
-            boolean equals = url.equals(request.getRequestURI());
-            if (equals){
+
+        String requestURI = request.getRequestURI();
+
+        // 使用AntPathMatcher進行路徑匹配
+        for (String url : ignoreUrls) {
+            if (antPathMatcher.match(url, requestURI)) {
                 return true;
             }
         }
+
         if (!ObjectUtils.isEmpty(loginAlready)) {
             return true;
         }
